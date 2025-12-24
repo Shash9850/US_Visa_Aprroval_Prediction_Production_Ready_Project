ec08898fa030846c34093fc2dcb1905e1fa8eb44
Shash9850 <moreshashikant332@gmail.com>
2025-12-24 16:35:04 +0530

    MongoDB Demo added.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="US_Visa",
+    vesion="0.0.0",
+    author="Shashikant",
+    author_email="moreshashikant332@gmail.com",
+    packages=find_packages()
+)
